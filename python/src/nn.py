918d77db4e737033cd38fa3c18c4dd4c19cfe3c5
Takuto Jibiki <eipuuuuk825@gmail.com>
2021-01-08 04:20:16 +0900

    nn.py に関して，相関係数と散布図を表示するようにした

--- python/src/nn.py
+++ python/src/nn.py
@@ -2,9 +2,12 @@
 # 7763c160b4ec1caa99718cd3c865339227a1908e
 
 import numpy as np
+import matplotlib.pyplot as plt
 import so
 
 # 入力された文字列が数値を表すかを判定
+
+
 def is_num(s):
     try:
         float(s)
@@ -48,10 +51,10 @@ def main():
     #
     # NN
     #
-    # nn = so.NeuralNetwork([18, 10, 2], 0.1, "regression")
-    # nn.learning(data_training_x, data_training_t, "data/E.csv", "data/param.csv")
-    nn = so.NeuralNetwork("regression", "param.csv")
-    nn.compute([float(i) for i in range(10)])
+    nn = so.NeuralNetwork([18, 10, 2], 0.1, "regression")
+    nn.learning(data_training_x, data_training_t,
+                "data/E.csv", "data/param.csv")
+    # nn = so.NeuralNetwork("regression", "param.csv")
 
     # 精度を検証
     predict_x = []
@@ -70,17 +73,44 @@ def main():
     # 相関係数
     coef_x = np.corrcoef(result[:, 0:2].T)[0, 1]
     coef_t = np.corrcoef(result[:, 2:].T)[0, 1]
-    print(coef_x, coef_t)
+    # print(coef_x, coef_t)
 
     # ファイルに出力
     with open("data/result.csv", "w") as fileobj:
-        fileobj.write("true x, predict x, true t, predict t\n")
+        fileobj.write("true x, predict x, true t, predict t,,")
+        fileobj.write("coef x,"+str(coef_x)+",coef t,"+str(coef_t)+"\n")
         for i in result:
             output_str = str(i[0])+","
             output_str += str(i[1])+","
             output_str += str(i[2])+","
             output_str += str(i[3])+"\n"
             fileobj.write(output_str)
+
+    #
+    # 散布図を描画
+    #
+    fig = plt.figure(figsize=(12, 6))
+    # x
+    xy_max = np.max(result[:, 0:2])
+    xy_min = np.min(result[:, 0:2])
+    g_x = fig.add_subplot(1, 2, 1)
+    plt.scatter(result[:, 0:1], result[:, 1:2])
+    plt.plot([xy_min, xy_max], [xy_min, xy_max], color="orange")
+    plt.title("predict x ("+f'{coef_x:.3f}'+")")
+    plt.xlabel("true x [px]")
+    plt.ylabel("predict x [px]")
+    plt.grid(True)
+    # t
+    xy_max = np.max(result[:, 2:4])
+    xy_min = np.min(result[:, 2:4])
+    g_t = fig.add_subplot(1, 2, 2)
+    plt.scatter(result[:, 2:3], result[:, 3:4])
+    plt.plot([xy_min, xy_max], [xy_min, xy_max], color="orange")
+    plt.title("predict t ("+f'{coef_t:.3f}'+")")
+    plt.xlabel("true t [sec]")
+    plt.ylabel("predict t [sec]")
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == '__main__':
